a28871d8cb4c399ece2e55e923b14056c7883d99
Leonardo Müller <leonardo.muller@engenharia.ufjf.br>
2024-12-27 16:11:03 -0300

    fix: init database correct

--- server/database/db.go
+++ server/database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -10,10 +11,33 @@ import (
 var DB *sql.DB
 
 func Init() (*sql.DB, error) {
-	DB, err := sql.Open("sqlite3", "../data/mydatabase.db")
+	DB, err := sql.Open("sqlite3", "data/mydatabase.db")
 	if err != nil {
 		log.Fatalf("failed to start sqlite: %v", err)
 		return nil, err
 	}
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("failed to start sqlite: %v", err)
+		return nil, err
+	}
+	err = runSQLFile(DB, "data/init.sql")
+	if err != nil {
+		log.Fatalf("failed to execute SQL file: %v", err)
+		return nil, err
+	}
 	return DB, nil
+}
+
+func runSQLFile(db *sql.DB, filePath string) error {
+	sqlBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(string(sqlBytes))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
